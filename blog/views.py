bd354a2f83ab4eb92aed789c05e27cdca3bc7ba2
AITI Student <aiti-student@AITI-80.(none)>
2012-07-10 15:28:05 +0000

    part3

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,29 @@
 # Create your views here.
+"""
+This code should be copied and pasted into your blog/views.py file before you begin working on it.
+"""
+
+from django.template import Context, loader
+from django.http import HttpResponse
+
+from models import Post, Comment 
+
+
+
+def post_list(request):
+    post_list = Post.objects.all()
+    
+    print type(post_list)
+    print post_list
+    
+    return HttpResponse('post_list')
+
+def post_detail(request, id, showComments=False):
+    pass
+ 
+def post_search(request, term):
+    pass
+   
+def home(request):
+    print 'it works'
+    return HttpResponse('hello world. Ete zene?') 
